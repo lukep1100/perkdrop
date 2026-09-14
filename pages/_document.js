5c83ad5c20a51790d138d4a4f72cbebb737e0b56
Luke Pranter <lukepranter11@gmail.com>
2026-09-14 23:27:36 +0930

    Make PerkDrop install metadata global

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
-  return <Html lang="en-AU"><Head /><body><Main /><NextScript /></body></Html>;
+  return <Html lang="en-AU"><Head>
+    <link rel="manifest" href="/manifest.webmanifest" />
+    <meta name="theme-color" content="#08090e" />
+    <meta name="mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+    <meta name="apple-mobile-web-app-title" content="PerkDrop" />
+    <link rel="apple-touch-icon" href="/icon.svg" />
+  </Head><body><Main /><NextScript /></body></Html>;
 }
